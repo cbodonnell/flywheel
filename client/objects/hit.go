b8b540c5ea3059cfb4976d335e75379c2cc668ea
Craig O'Donnell <cbodonnell@comcast.net>
2024-04-29 21:39:57 -0400

    code clean up

--- client/objects/hit.go
+++ client/objects/hit.go
@@ -10,37 +10,56 @@ import (
 	"golang.org/x/image/font"
 )
 
-type Hit struct {
+type TextEffect struct {
 	*BaseObject
 
-	ID   string
-	text string
-	x    float64
-	y    float64
+	ID     string
+	text   string
+	x      float64
+	y      float64
+	color  color.Color
+	scroll bool
 }
 
-func NewHit(id string, text string, x, y float64) *Hit {
-	return &Hit{
+type NewTextEffectOptions struct {
+	Text   string
+	X      float64
+	Y      float64
+	Color  color.Color
+	Scroll bool
+}
+
+func NewTextEffect(id string, opts NewTextEffectOptions) *TextEffect {
+	clr := opts.Color
+	if clr == nil {
+		clr = color.White
+	}
+
+	return &TextEffect{
 		BaseObject: NewBaseObject(id),
 		ID:         id,
-		text:       text,
-		x:          x,
-		y:          y,
+		text:       opts.Text,
+		x:          opts.X,
+		y:          opts.Y,
+		color:      opts.Color,
+		scroll:     opts.Scroll,
 	}
 }
 
-func (p *Hit) Update() error {
-	factor := float64(ebiten.TPS()) / 60
-	p.y += 1 * factor
+func (o *TextEffect) Update() error {
+	if o.scroll {
+		factor := float64(ebiten.TPS()) / 60
+		o.y += 1 * factor
+	}
 	return nil
 }
 
-func (p *Hit) Draw(screen *ebiten.Image) {
-	t := strings.ToUpper(p.text)
+func (o *TextEffect) Draw(screen *ebiten.Image) {
+	t := strings.ToUpper(o.text)
 	f := fonts.TTFSmallFont
 	bounds, _ := font.BoundString(f, t)
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(p.x-float64(bounds.Max.X>>6)/2, float64(screen.Bounds().Dy())-p.y)
-	op.ColorScale.ScaleWithColor(color.RGBA{255, 0, 0, 255}) // red
+	op.GeoM.Translate(o.x-float64(bounds.Max.X>>6)/2, float64(screen.Bounds().Dy())-o.y)
+	op.ColorScale.ScaleWithColor(o.color)
 	text.DrawWithOptions(screen, t, f, op)
 }
